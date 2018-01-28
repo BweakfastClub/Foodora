0feb955af6feefff5ef6443fe82ee46dd4a7c118
Kyle Kung <kkung065@uottawa.ca>
2018-01-28 14:03:54 -0500

    playing with cassandra

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,22 @@
+
 module.exports.findAllUsers = (req, res) => {
-    res.status(202).json({data : "success"})
+	const cassandra = require('cassandra-driver');
+	const client = new cassandra.Client({
+		contactPoints: ['127.0.0.1']
+	});
+
+	client.connect()
+	  .then(function () {
+	    console.log('Connected to cluster with %d host(s): %j', client.hosts.length, client.hosts.keys());
+	    console.log('Keyspaces: %j', Object.keys(client.metadata.keyspaces));
+	    console.log('Shutting down');
+	    return client.shutdown();
+	  })
+	  .catch(function (err) {
+	    console.error('There was an error when connecting', err);
+	    return client.shutdown();
+	  });
+    // res.status(202).json({data : "success"})
 }
 
 module.exports.register = (req, res) => {
