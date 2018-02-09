78b2d9d63656e9cf2bcc897cb6771aa90760fcad
Kyle Kung <kkung065@uottawa.ca>
2018-02-09 11:53:06 -0500

    add test suite for post users endpoint

--- test/controllers/users_controller_test.js
+++ test/controllers/users_controller_test.js
@@ -19,7 +19,52 @@ describe("Endpoints exists", () => {
     });
 
     describe("/POST users", () => {
-        it("the get users endpoint should exist", (done) => {
+        it("Post user requires name", (done) => {
+            chai.request(usersRoutes).
+                post("/users").
+                set("content-type", "application/json").
+                send({
+                    email: "user@email.com",
+                    password: "1234"
+                }).
+                end((err, res) => {
+                    should.exist(err);
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+
+        it("Post user requires email", (done) => {
+            chai.request(usersRoutes).
+                post("/users").
+                set("content-type", "application/json").
+                send({
+                    email: "user@email.com",
+                    name: "user"
+                }).
+                end((err, res) => {
+                    should.exist(err);
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+
+        it("Post user requires password", (done) => {
+            chai.request(usersRoutes).
+                post("/users").
+                set("content-type", "application/json").
+                send({
+                    name: "user",
+                    password: "1234"
+                }).
+                end((err, res) => {
+                    should.exist(err);
+                    res.should.have.status(400);
+                    done();
+                });
+        });
+
+        it("Post user successful", (done) => {
             chai.request(usersRoutes).
                 post("/users").
                 set("content-type", "application/json").
