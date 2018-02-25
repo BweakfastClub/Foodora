a5a73e481340dd0ff9c4972b9c56199adb3a6ae7
Kyle Kung <kkung065@uottawa.ca>
2018-02-25 17:28:36 -0500

    add test to spawn child process in python

--- src/controllers/recipes_controller.js
+++ src/controllers/recipes_controller.js
@@ -1,4 +1,5 @@
 const recipesModel = require("../models/recipes_model");
+const {spawn} = require("child_process");
 
 module.exports.setUp = () => {
     recipesModel.setup();
@@ -6,7 +7,15 @@ module.exports.setUp = () => {
 
 module.exports.findAllRecipes = (req, res) => {
     recipesModel.selectAllRecipes((err, result) => {
-        console.log(err)
+        console.log(err);
         res.status(200).json(result);
+    });
+};
+
+module.exports.callPythonScriptTest = (req, res) => {
+    const pythonProcess = spawn("python", ["test_script.py"]);
+
+    pythonProcess.stdout.on("data", (data) => {
+        res.status(200).json(data.toString());
     });
 };
---
+++ test_script.py
@@ -0,0 +1,7 @@
+import sys
+
+def main():
+    print("Hello")
+
+if __name__ == '__main__':
+    main()
