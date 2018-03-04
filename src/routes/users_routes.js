8dbfcf1e9e112a2b1c52cce38d17ca9fc1820a4f
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2018-03-04 16:04:40 -0500

    Clean db and setup test env for travis

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -1,7 +1,11 @@
+require("dotenv/config");
+
 const app = require("express").Router();
 const usersController = require("../controllers/users_controller");
 
-usersController.setUp();
+if (process.env.NODE_ENV !== "testing") {
+    usersController.setUp();
+}
 
 app.get("/", usersController.findAllUsers);
 app.post("/", usersController.register);
